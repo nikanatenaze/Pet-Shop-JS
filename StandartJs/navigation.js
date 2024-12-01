1a7d9f16bc53331fdbd2e1068b133a76286389e4
nika natenadze <nikanatenaze@gmail.com>
2024-12-02 03:46:59 +0400

    Update
    added reponsability(for navigation and home page). fixed bugs

---
+++ StandartJs/navigation.js
@@ -0,0 +1,6 @@
+let burgerbar = document.getElementById("burgerbar");
+let slide = document.getElementById("slider")
+
+burgerbar.addEventListener('click', () => {
+    slide.classList.toggle("slided")
+}) 
